34bf3906498d3e6a849e21039f18c44776777be8
Tohir Omoloye <otcleantech@gmail.com>
2021-08-27 01:12:33 +0100

    hide secrets

--- download.py
+++ download.py
@@ -5,8 +5,8 @@ session = boto3.session.Session()
 client = session.client('s3',
                         region_name='sfo2',
                         endpoint_url='https://sfo2.digitaloceanspaces.com',
-                        aws_access_key_id='ZMV4HG2GSLWU45PLDW3A',
-                        aws_secret_access_key='M8KEzMvKhXfrfld0eyRJCwmYaeRjsBES/6ceiMmdZY0')
+                        aws_access_key_id='',
+                        aws_secret_access_key='')
 
 client.download_file('zend-storage',
                      'documents/19/5gZ1iTwyPhzRUGQ9cttqt6C16kRrg700gJxKDCMn.pdf',
